93a925cd569a3c8b0451dc2a2f2e9d074f7bcf8a
A64Danil <a64danil@mail.ru>
2018-03-02 22:21:18 +0300

    сделали частично днд на своём проекте

--- src/my.js
+++ src/my.js
@@ -6,11 +6,10 @@ console.log("NODE JS are working. Русские символы 3");
 var divCreator = document.body.querySelector('.divCreator')
 var divCrasher = document.body.querySelector('.divCrasher')
 var divPool = document.body.querySelector('.divPool')
- console.log(divPool);
+console.log(divPool);
 
 
 var uniqueCounter = 1;
-
 
 divCreator.addEventListener('click', createDiv)
 divCrasher.addEventListener('click', crashDiv)
@@ -41,6 +40,7 @@ function createDiv(e) {
     newDiv.className += ' '+'block'+ uniqueCounter;
 
     divPool.insertBefore(newDiv, divPool.firstChild);
+    addListeners(newDiv);
     ++uniqueCounter;
 }
 
@@ -51,4 +51,51 @@ function crashDiv(e) {
     console.log(uniqueCounter);
     divPool.removeChild(deletedDiv);
     deletedDiv = null;
+}
+
+function addListeners(target) {
+    //target.addEventListener('click', clrCnhg);
+    target.addEventListener('mousedown', dnd);
+
+    function clrCnhg(e) {
+
+        //console.log(e.currentTarget.className);
+        let rndColor = Math.floor(Math.random()*16777215).toString(16);
+        e.currentTarget.style.backgroundColor= '#' + rndColor;
+    }
+
+    function dnd(e) {
+        console.log("DND" + e.currentTarget.className);
+
+        e.currentTarget.style.zIndex = uniqueCounter + 1000;
+
+        let shiftX = (e.pageX - 67) - parseInt(target.style.left);
+        let shiftY = (e.pageY - 182) - parseInt(target.style.top);
+
+        console.log("Сдвиг мыши по Х" + shiftX);
+        console.log("Сдвиг мыши по Y" + shiftY);
+
+        function moveTo(e) {
+            target.style.left = e.pageX - 67 - shiftX + 'px';
+            target.style.top = e.pageY - 182 - shiftY + 'px';
+            //console.log("мышь по X" + e.clientX);
+            //console.log("мышь по Y" + e.clientY);
+
+
+        }
+
+        document.body.addEventListener('mousemove', moveTo);
+
+        document.body.addEventListener('mouseup', function(e) {
+            document.body.removeEventListener('mousemove', moveTo);
+            console.warn("Отпустили мыш");
+            //target.style.zIndex -= 1000;
+            //ball.onmouseup = null;
+        });
+
+
+
+    }
+
+
 }
